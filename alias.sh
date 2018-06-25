c6c37d9867913e9e57ad2e6b0387bbfabd512d65
Erik Örjehag <erik@orjehag.se>
2018-06-25 23:08:45 +0200

    initial

---
+++ alias.sh
@@ -0,0 +1,2 @@
+alias dmake='( cd ~/catkin_ws && catkin_make )'
+alias ddep='rosdep install --from-paths ~/catkin_ws/src --ignore-src -r -y'
---
+++ rosenv-templ.sh
@@ -0,0 +1,9 @@
+source ~/catkin_ws/devel/setup.bash
+source ~/Code/dyno-shell/alias.sh
+
+ROSCONSOLE_CONFIG_FILE=~/Code/dyno-shell/rosconsole.config
+
+# export ROS_IP=???
+# export ROS_MASTER_URI=???
+
+# export DYNO_BASE=omnibot
