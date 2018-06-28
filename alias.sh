75cd4044867a1120cbb62d02a6aa9b5f49360f95
Erik Örjehag <erik@orjehag.se>
2018-06-28 22:51:19 +0200

    can now pass -j1 to dmake

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
-alias dmake='( cd ~/catkin_ws && catkin_make )'
+alias dmake='function _blah(){ ( cd ~/catkin_ws && catkin_make $1 ) };_blah'
 alias ddep='rosdep install --from-paths ~/catkin_ws/src --ignore-src -r -y'
